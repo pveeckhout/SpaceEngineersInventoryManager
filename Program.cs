7b955d5cb8322c5cf334effb9c00299adf799c32
Pieter Van Eeckhout <vaneeckhout.pieter@gmail.com>
2015-12-27 15:09:23 +0100

    starting project, adding libs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Sandbox.ModAPI.Ingame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceEngineersInventoryManager
+{
+    class Program
+    {
+
+        #region programming environment essential inits, DO NOT COPY TO GAME
+        IMyGridTerminalSystem GridTerminalSystem;
+        #endregion
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
